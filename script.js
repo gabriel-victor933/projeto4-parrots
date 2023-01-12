b2533a179d8ca9e77308f49f801c0649a05ada47
gabriel-victor9333 <gabrielvictor933@gmail.com>
2023-01-12 09:34:38 -0300

    cartas agora possuem frente e verso

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-
-let Ncartas = 5;
+//variaveis globais
+let Ncartas = 14;
+let NcartasPossiveis = 14;
 
 do {
     Ncartas = prompt("Qual o numero de cartas que deseja jogar?");
 } while (Ncartas % 2 != 0 || Ncartas < 4 || Ncartas > 14);
+
+
+//remove as cartas que não serão usadas;
+let cartas = document.querySelectorAll(".carta");
+for (let i = 0; i != NcartasPossiveis - Ncartas; i++) {
+    console.log(cartas[NcartasPossiveis - i - 1]);
+    cartas[NcartasPossiveis - i - 1].classList.add("carta-fora");
+}
+
+
 
 
 
